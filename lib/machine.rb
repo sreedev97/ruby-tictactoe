624a47fda90fde78adc85f77697c533f1b88464c
Sreedev Kodichath <sreedev@icloud.com>
2018-12-02 20:44:53 +0530

    added attack to machine gameplay

--- lib/machine.rb
+++ lib/machine.rb
@@ -1,7 +1,9 @@
+require 'pry'
 class Machine
   def play(board)
     return false if board.draw? || board.won?
-    row, col = generate_coordinates(board)
+    gameMode = gameModeChoice(board)
+    row, col = gameMode == :defense ? defense_coordinates(board) : attack_coordinates(board)
     if board.rows[row][col] == '.'
       board.markBoard(row.next, col.next)
       board.switchPlayer
@@ -11,27 +13,55 @@ class Machine
     end
   end
 
-  def generate_coordinates(board)
-    possible_patterns = win_moves(board)
-    expected_pattern = possible_patterns[possible_patterns.keys.max]
-    mapped_expected_pattern = expected_pattern.map do |position|
-      {position => board.rows[position.first][position.last]}
+  def gameModeChoice(board)
+    attack_choices, defense_choices = attack_moves(board), defense_moves(board)
+    binding.pry
+    if !attack_choices.is_a?(Hash) || attack_choices.keys.max.to_i < defense_choices.keys.max.to_i
+      return :defense
+    else
+      return :attack
     end
-    selected_position = mapped_expected_pattern.select{|position| position.values.first == '.'}.first
-    return selected_position.keys.first
   end
 
-  def win_moves(board)    
-    win_possibilities = {}
+  def defense_coordinates(board)
+    compute_coordinates(board, :defense) 
+  end
+
+  def attack_coordinates(board)
+    compute_coordinates(board, :attack)
+  end
+
+  def attack_moves(board)
+    compute_moves(board, board.current_player)  
+  end
+
+  def defense_moves(board)    
+    compute_moves(board, board.opponent) 
+  end
+
+  def compute_moves(board, player)
+    win_possibilities = Hash.new
     board.winPatterns.each do |pattern|
       pattern_map = pattern.map do |row, column|
         board.rows[row][column]
       end
-      if pattern_map.include?(board.opponent) && pattern_map.include?('.') # !pattern_map.include?(board.current_player)
-        pattern_possibility = (pattern_map.count(board.opponent)/3.0)*100
+      if pattern_map.include?(player) && pattern_map.include?('.') # !pattern_map.include?(board.current_player)
+        pattern_possibility = (pattern_map.count(player)/3.0)*100
         win_possibilities.merge!(pattern_possibility => pattern)
       end
     end
     return win_possibilities
   end
+
+  def compute_coordinates(board, gameMode)
+    possible_patterns = gameMode == :defense ? defense_moves(board) : attack_moves(board)
+    expected_pattern = possible_patterns[possible_patterns.keys.max]
+    return false if expected_pattern.nil?
+    mapped_expected_pattern = expected_pattern.map do |position|
+     {position => board.rows[position.first][position.last]}
+    end
+    selected_position = mapped_expected_pattern.select{|position| position.values.first == '.'}.first
+    return selected_position.keys.first
+  end
+
 end
